aac2d38b4ba02c31827f7a442a67a2cc7c2184db
bilgecicek99 <83015725+bilgecicek99@users.noreply.github.com>
2023-04-17 09:33:59 +0300

    düzenlemeler eklendi

--- src/AdminLogin.js
+++ src/AdminLogin.js
@@ -1,52 +1,55 @@
-import ReactDOM from 'react-dom';
-import { Redirect } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route, Navigate, Redirect } from 'react-router-dom';
 import Adminpanel from './Adminpanel';
 import React, { useState } from 'react';
 
 export default function AdminLogin() {
 
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
+  const [redirectToAdminpanel, setRedirectToAdminpanel] = useState(false);
+  const [isError, setIsError] = useState(false);
 
-   
+  const handleSubmit = async (event) => {
+    event.preventDefault();
+    // burada, kullanıcı adı ve şifreyi bir API'ye gönderip doğrulayabilirsiniz
+    // bu örnekte, doğrulama her zaman başarılı olur
+    setRedirectToAdminpanel(true);
+  };
 
-    
-      const [username, setUsername] = useState('');
-      const [password, setPassword] = useState('');
-      const [redirectToAdmin, setRedirectToAdmin] = useState(false);
-    
-      const handleSubmit = async (event) => {
-        event.preventDefault();
-        // burada, kullanıcı adı ve şifreyi bir API'ye gönderip doğrulayabilirsiniz
-        // bu örnekte, doğrulama her zaman başarılı olur
-        setRedirectToAdmin(true);
-      };
-      if (redirectToAdmin) {
-        return <Redirect to="/Adminpanel" />;
-      }
-    
-      return (
-        <form onSubmit={handleSubmit}>
-          <label>
-            Kullanıcı Adı:
-            <input
-              type="text"
-              value={username}
-              onChange={(event) => setUsername(event.target.value)}
-            />
-          </label>
-          <label>
-            Şifre:
-            <input
-              type="password"
-              value={password}
-              onChange={(event) => setPassword(event.target.value)}
-            />
-          </label>
-          <button type="submit">Giriş Yap</button>
-        </form>
-      );
-    };
-    
-    
+  const handleAdminPanelLoad = () => {
+    try {
+      // Burada Adminpanel bileşenini yükleyebilirsiniz. Eğer bir hata oluşursa, catch bloğunda işlenecektir.
+      return <Adminpanel />
+    } catch (error) {
+      setIsError(true);
+      return null;
+    }
+  };
 
-    
+  if (redirectToAdminpanel) {
+    return <Redirect to="/adminpanel" />;
+  }
 
+  return (
+    <form className='admisgiris'  onSubmit={handleSubmit}>
+      <label>
+        Kullanıcı Adı:
+        <input
+          type="text"
+          value={username}
+          onChange={(event) => setUsername(event.target.value)}
+        />
+      </label>
+      <label>
+        Şifre:
+        <input
+          type="password"
+          value={password}
+          onChange={(event) => setPassword(event.target.value)}
+        />
+      </label>
+      <button type="submit">Giriş Yap</button>
+      {isError && <p>Hata oluştu.</p>}
+    </form>
+  );
+}
--- src/Card.js
+++ src/Card.js
@@ -1,34 +1,55 @@
-import React from "react";
-import CardItem from "./CardItems";
-function Card() {
-  return (
-    <div className="cards">
-      <div className="cards__container">
-        
-        <div className="cards__wrapper">
-          <ul className="cards__items">
-            <CardItem
-              src="images/saksı.png"
-              text="kendimize özel çiçek"
-              path=""
-              desc="gelecek"
-            />
-            <CardItem
-              src="/images/saksı.png"
-              text="çiçek gelcek"
-              path=""
-              desc="çiçek gelcek"
-            />
-            <CardItem
-              src="/images/saksı.png"
-              text="saksı"
-              path=""
-              desc="saksı gelcek"
-            />
-          </ul>
+  import React from "react";
+  import CardItem from "./CardItems";
+
+  function Card() {
+    return (
+      <div className="cards">
+        <div className="cards__container">
+          <div className="cards__wrapper">
+            <ul className="cards__items">
+              <CardItem
+                src="images/saksı.png"
+                text="Saksı"
+                path=""
+                desc="yükleniyor"
+              />
+              <CardItem
+                src="/images/saksı.png"
+                text="Kurutulmuş Çiçek"
+                path=""
+                desc="yükleniyor"
+              />
+              <CardItem
+                src="/images/saksı.png"
+                text="Büyük Bitki"
+                path=""
+                desc="yükleniyor"
+              />
+            </ul>
+            <ul className="cards__items">
+              <CardItem
+                src="/images/saksı.png"
+                text="Aranjmanlar"
+                path=""
+                desc="yükleniyor"
+              />
+              <CardItem
+                src="/images/saksı.png"
+                text="Kendin Yap"
+                path=""
+                desc="yükleniyor"
+              />
+              <CardItem
+                src="/images/saksı.png"
+                text="x"
+                path=""
+                desc="yükleniyor"
+              />
+            </ul>
+          </div>
         </div>
       </div>
-    </div>
-  );
-}
-export default Card;
+    );
+  }
+
+  export default Card;
