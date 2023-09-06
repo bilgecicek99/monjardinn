7ec25c670dae185c704187cf49d33e259de1d7de
Hilal B <128605658+HilalBastanAppelier@users.noreply.github.com>
2023-09-06 17:10:48 +0300

    blog silme eklendi

--- src/admin/AdminAllBlog.js
+++ src/admin/AdminAllBlog.js
@@ -2,6 +2,9 @@ import React, { useState,useEffect } from "react";
 import WithNavbar from '../WithNavbar'; 
 import { useNavigate } from 'react-router-dom';
 import { baseUrl } from '../config/Constants';
+import { getToken } from "../service/AuthService";
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 
 const Navbar = ({ onSearch }) => {
   
@@ -38,64 +41,31 @@ const Navbar = ({ onSearch }) => {
   );
 };
 
-const BlogCard = ({ id,title, image, likes, caption }) => {
-  const navigate = useNavigate();
-  const defaultImage = '/images/monjardinlogo.png';
-
-  const handleEditClick = (blog) => {
-    console.log("blog", blog);
-    // Edit sayfasına yönlendirme işlemi
-    navigate(`/editblog/${blog}`, { state: { blog } });
-  };
-  const truncateText = (text, maxLength) => {
-    if (text.length > maxLength) {
-      return text.substring(0, maxLength) + "...";
-    }
-    return text;
-  };
-  
-  return (
-    <div style={{ margin: "auto", width: "300px", height: "400px", border: "none", borderRadius: "10px", padding: "10px", marginBottom: "20px", background:"#E7D1EA" }}>
-      <button  onClick={() => handleEditClick(id)} style={{height: "auto", width: "auto" , background: "transparent"}}>
-          <img src="/images/edit.png" alt="" width={28} height={28} />
-      </button>
-      <img src={image || defaultImage} alt={title} style={{ width: "100%", height: "200px",justifyContent:"center", objectFit: "cover",borderRadius:"10px",objectFit:"contain" }} />
-      <h3 style={{ fontFamily: 'Times New Roman', fontWeight: 'bold' }}>{title}</h3>
-      <p style={{ fontFamily: 'Times New Roman', fontStyle: 'italic',display: "inline-block",
-        wordBreak: "break-word",
-        tableLayout: "fixed" }}>{truncateText(caption, 100)}</p>      <hr/>
-     <div style={{float:"right"}}>
-    </div>
-    </div>
-  );
-};
 
 const AdminAllBlog = () => {
   const [allBlog, setAllBlog] = useState([]);
   const [filteredPosts, setFilteredPosts] = useState([]);
 
-  useEffect(() => {
-     
-    const fetchAllBlog = async () => {
-      try {
-        
-        const response = await fetch(baseUrl+`api/Blog/GetAllBlog`);
-        const data = await response.json();
-        if(data.success)
-        {
-          const blog = data.data;
-          setAllBlog(blog);
-          setFilteredPosts(blog)
-        }
-        
-      } catch (error) {
-        console.error("fetchBlog hatası:", error);
-        // Hata durumunda yapılacak işlemler
+  const fetchAllBlog = async () => {
+    try {
+      
+      const response = await fetch(baseUrl+`api/Blog/GetAllBlog`);
+      const data = await response.json();
+      if(data.success)
+      {
+        const blog = data.data;
+        setAllBlog(blog);
+        setFilteredPosts(blog)
       }
-    };
-  
-    fetchAllBlog();
-  }, []);
+      
+    } catch (error) {
+      console.error("fetchBlog hatası:", error);
+      // Hata durumunda yapılacak işlemler
+    }}
+  useEffect(() => {
+        fetchAllBlog()
+  }
+  ,[]);
 
   const handleSearch = (searchTerm) => {
     if (searchTerm === "") {
@@ -120,9 +90,111 @@ const AdminAllBlog = () => {
     navigate('/adminaddblog');
   }
 
+  const BlogCard = ({ id,title, image, likes, caption }) => {
+    const navigate = useNavigate();
+    const token = getToken();
+    const [isDeleteConfirmationVisible, setIsDeleteConfirmationVisible] = useState(false);
+  
+    const defaultImage = '/images/monjardinlogo.png';
+  
+    const handleEditClick = (blog) => {
+      console.log("blog", blog);
+      // Edit sayfasına yönlendirme işlemi
+      navigate(`/editblog/${blog}`, { state: { blog } });
+    };
+    const truncateText = (text, maxLength) => {
+      if (text.length > maxLength) {
+        return text.substring(0, maxLength) + "...";
+      }
+      return text;
+    };
+  
+    function showToast(message, duration = 3000) {
+      toast.error(message, {
+        position: toast.POSITION.TOP_CENTER,
+        autoClose: duration,
+        hideProgressBar: false,
+        closeOnClick: true,
+        pauseOnHover: true,
+      });
+    }
+  
+    const handleDeleteClick = (id) => {
+      fetch(baseUrl+`api/Blog/DeleteBlog?id=${id}`, {
+        method: "DELETE",
+        headers: {
+          Authorization: `Bearer ${token}`,
+        },
+      })
+        .then((response) => response.json())
+        .then((data) => {
+          if(data.success)
+          {
+            toast.success(data.message, {
+              position: toast.POSITION.TOP_CENTER,
+              autoClose: 2000,
+              hideProgressBar: false,
+              closeOnClick: true,
+              pauseOnHover: true,
+            });
+            fetchAllBlog();
+  
+          }
+          else{
+            showToast(data.message??'Lütfen Daha Sonra Tekrar Deneyiniz');
+          }
+        })
+        .catch((error) => {
+          showToast('Lütfen Daha Sonra Tekrar Deneyiniz');
+        });
+        setIsDeleteConfirmationVisible(false);
+  
+    }
+    
+    return (
+      <div style={{ 
+        margin: "auto", 
+        width: "300px", 
+        height: "400px", 
+        border: "none", 
+        borderRadius: "10px", 
+        padding: "10px", 
+        marginBottom: "20px", 
+        background:"#E7D1EA" }}>
+               <ToastContainer />
+          <div style={{ float: "right" }}>
+            <button  onClick={() => handleEditClick(id)} style={{height: "auto", width: "auto" , background: "transparent"}}>
+                <img src="/images/edit.png" alt="" width={28} height={28} />
+            </button>
+            <button style={{height: "auto", width: "auto" , background: "transparent"}}>
+                <img onClick={() => setIsDeleteConfirmationVisible(true)} src={"/images/delete.png"} alt="" width={23} height={23}/>
+            </button>
+            {isDeleteConfirmationVisible && (
+                <div className="delete-confirmation-overlay">
+                  <div className="delete-confirmation-box">
+                    <p>Silmek istediğinize emin misiniz?</p>
+                    <button onClick={()=> handleDeleteClick(id)}>Evet</button>
+                    <button onClick={() => setIsDeleteConfirmationVisible(false)}>Hayır</button>
+                  </div>
+                </div>
+              )}
+          </div>
+        <img src={image || defaultImage} alt={title} style={{ width: "100%", height: "200px",justifyContent:"center", objectFit: "cover",borderRadius:"10px",objectFit:"contain" }} />
+        <h3 style={{ fontFamily: 'Times New Roman', fontWeight: 'bold' }}>{title}</h3>
+        <p style={{ fontFamily: 'Times New Roman', fontStyle: 'italic',display: "inline-block",
+          wordBreak: "break-word",
+          tableLayout: "fixed" }}>{truncateText(caption, 100)}</p>      <hr/>
+       <div style={{float:"right"}}>
+      </div>
+      </div>
+    );
+  };
+
   return (
     <div  style={{ margin: "50px" }}>
       <h1  className='baslik'>Mon Jardin</h1>
+      <ToastContainer />
+
       <button onClick={handleGoBack} className='back-button'><img src="/images/back-button.png" alt="" width={40} height={30}/></button>
 
       <button
