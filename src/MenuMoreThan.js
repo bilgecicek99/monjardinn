b977a1e0a20c7e6920c230c1f75c898159917a9c
SenaOzturkk <89801269+SenaOzturkk@users.noreply.github.com>
2023-10-26 16:30:25 +0300

    footera ikonar ve menu

--- src/MenuMoreThan.js
+++ src/MenuMoreThan.js
@@ -1,25 +1,34 @@
-  import React, { useState, useEffect } from "react";
-  import WithNavbar from './WithNavbar'; 
+import React, { useState, useEffect } from "react";
+import WithNavbar from './WithNavbar'; 
 
+const MenuMoreThan = () => {
+  const [selectedItem, setSelectedItem] = useState(0);
 
-  const MenuMoreThan = () => {
-      const [selectedItem, setSelectedItem] = useState(0);
+  useEffect(() => {
+    // Sayfa yüklendiğinde URL'deki anker kısmını kontrol ederek ilgili öğeyi seç
+    const currentHash = window.location.hash;
+    const selectedItemIndex = currentHash ? listItems.findIndex(item => `#${item.id}` === currentHash) : 0;
+    setSelectedItem(selectedItemIndex);
+  }, []); // Boş bağımlılık dizisi sayfa yüklendiğinde bir kez çalışır
 
-      const listItems = [{title:'Mesafeli Satış Sözleşmeleri',description:'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.'}, 
-      {title:'Gizlilik Politikası',description:'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.'}, 
-      {title:'KVKK Sözleşmesi',description:'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.'}, 
-      {title:'Teslimat-İade Şartları',description:'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.'}];
-    
-      const handleItemClick = (index) => {
-        setSelectedItem(index);
-      };
-    
-    return (
-      <div style={{ marginTop: "100px" }}>
-      
+  const listItems = [
+    {id: 'hakkimizda', title:'Hakkımızda', description:'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.'},
+    {id: 'satis-sozlesmesi', title:'Mesafeli Satış Sözleşmeleri', description:'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.'}, 
+    {id: 'gizlilik-politikasi', title:'Gizlilik Politikası', description:'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.'}, 
+    {id: 'teslimat-iade-bildirimi', title:'Teslimat-İade Bildirimi', description:'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.'}
+  ];
+
+  const handleItemClick = (index) => {
+    setSelectedItem(index);
+    // Sayfa içeriği değiştiğinde URL'deki anker kısmını güncelle
+    window.location.hash = `#${listItems[index].id}`;
+  };
+
+  return (
+    <div style={{ marginTop: "100px" }}>
       <div className="container">
         <div className="left-panel">
-          <ul style={{paddingLeft:0}}>
+          <ul style={{ paddingLeft: 0 }}>
             {listItems.map((item, index) => (
               <li
                 key={index}
@@ -37,9 +46,8 @@
           <p style={{textAlign:"left"}}>{listItems[selectedItem].description}</p>
         </div>
       </div>
+    </div>
+  );
+};
 
-      </div>
-    );
-  };
-
-  export default WithNavbar(MenuMoreThan);
+export default WithNavbar(MenuMoreThan);
